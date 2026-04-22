2431960f9bd1fd67960b6019401c93368a9df24d
Xesderte <sardinamc1514@gmail.com>
2026-04-22 16:10:45 -0300

    Generar colores HSL, y luego pasarlos a HEX

--- app.js
+++ app.js
@@ -2,17 +2,37 @@
 const boton = document.getElementById("generarBtn");
 const contenedor = document.getElementById("contenedor");
 
-// Función para generar color HEX
-function generarColorHex() {
-    const letras = "0123456789ABCDEF";
-    let color = "#";
+// Función para generar color HSL aleatorio
+function generarColorHSL() {
+    const h = Math.floor(Math.random() * 360);
+    const s = 70;
+    const l = 60;
 
-    for (let i = 0; i < 6; i++) {
-        const random = Math.floor(Math.random() * 16);
-        color += letras[random];
-    }
+    colorHsl =`hsl(${h}, ${s}%, ${l}%)`;
 
-    return color;
+    return {colorHsl, h, s, l };
+}
+
+//Funcion para convertir HSL a Hexadecimal
+function hslToHex(h, s, l) {
+    // Convertir a RGB primero
+    s /= 100;
+    l /= 100;
+
+    const k = n => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+
+    const f = n =>
+        l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+
+    const r = Math.round(255 * f(0));
+    const g = Math.round(255 * f(8));
+    const b = Math.round(255 * f(4));
+
+    // Convertir a HEX
+    const toHex = x => x.toString(16).padStart(2, "0");
+
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`.toUpperCase();
 }
 
 // Evento click
@@ -25,18 +45,21 @@ boton.addEventListener("click", () => {
     // Generar 3 colores (fase 1)
     for (let i = 0; i < 3; i++) {
 
-        const color = generarColorHex();
-        console.log(color);
+        const {colorHsl, h, s, l} = generarColorHSL();
+        console.log(colorHsl);
 
         // Crear elemento
         const caja = document.createElement("div");
         caja.classList.add("color");
 
         // Aplicar color
-        caja.style.backgroundColor = color;
+        caja.style.backgroundColor = colorHsl;
 
+        //Pasar de HSL a Hexadecimal
+        const colorHex = hslToHex(h, s, l);
+        
         // Mostrar texto
-        caja.textContent = color;
+        caja.textContent = colorHex;
 
         // Agregar al DOM
         contenedor.appendChild(caja);
